07c8dd525d037ea783e7ed85a7595e1e2b9799b9
thibault rahou <trahou33@gmail.com>
2020-06-29 17:59:16 +0200

    controller, validation, code action cable

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,4 +1,7 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
+    def subscribed
+      stream_from "chatroom_#{params[:chatroom_id]}"
+    end
   end
 end
---
+++ app/javascript/action_cable/index.js
@@ -0,0 +1,7 @@
+import {scrollLastMessageIntoView} from "./scroll.js"
+// import {errorMessageIntoView} from "./create.js.erb"
+
+scrollLastMessageIntoView();
+// errorMessageIntoView();
+
+console.log("ok")
