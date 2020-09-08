ce38abebc510365e9d1cc3a874d4c872b0462af2
Valentin Lassartesse <valentin.lassartesse@gmail.com>
2020-09-08 15:41:55 +0200

    ajax on chat works now, thx turbolink

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
     @chatroom = Chatroom.find(params[:chatroom_id])
     @message = Message.new(message_params)
@@ -6,7 +8,7 @@ class MessagesController < ApplicationController
     @message.user = current_user
     if @message.save
       ChatroomChannel.broadcast_to(@chatroom, render_to_string(partial: "message", locals: { message: @message }))
-      redirect_to user_chatrooms_path(@chatroom, anchor: "message-#{message.id}")
+      # redirect_to user_chatrooms_path(@chatroom, anchor: "message-#{@message.id}")
     else
       render "chatrooms/show"
     end
