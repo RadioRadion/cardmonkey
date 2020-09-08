32b1639101b86c81cfd3623f935e925c4e1bc1b3
Valentin Lassartesse <valentin.lassartesse@gmail.com>
2020-09-08 09:57:11 +0200

    #create of trade finish with messages and chatrrom

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,19 +1,14 @@
 import consumer from "./consumer";
 import scroll from "./scroll"
 
-const initChatroomCable = () => {
-  const messagesContainer = document.getElementById('messages');
-  if (messagesContainer) {
-    const id = messagesContainer.dataset.chatroomId;
+const messagesContainer = document.getElementById('messages');
+if (messagesContainer) {
+  const id = messagesContainer.dataset.chatroomId;
 
-    consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
-      received(data) {
-        console.log(data); // called when data is broadcast in the cable
-        messagesContainer.insertAdjacentHTML('beforeend', data);
-      },
-    });
-  }
+  consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
+    received(data) {
+      console.log(data); // called when data is broadcast in the cable
+      messagesContainer.insertAdjacentHTML('beforeend', data);
+    },
+  });
 }
-
-export { initChatroomCable };
-
