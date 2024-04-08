9949936d1798e90b55b40e9fefb4b20537e55a90
RadioRadion <valentin.lassartesse@gmail.com>
2024-04-08 12:15:09 +0200

    save before adding extension table

---
+++ db/migrate/20240408101157_add_extension.rb
@@ -0,0 +1,16 @@
+class AddExtensionToCardVersions < ActiveRecord::Migration[7.1]
+  
+  def change
+    create_table :extensions do |t|
+      t.string :code, null: false
+      t.string :name
+      t.date :release_date
+      t.string :icon_uri
+
+      t.timestamps
+
+    add_index :extensions, :code, unique: true
+
+    add_reference :card_versions, :extension, null: true, foreign_key: true
+  end
+end
