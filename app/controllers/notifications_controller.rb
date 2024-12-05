0486f6a32ce046ed71b8b0958e7c0e114d19b5b6
RadioRadion <valentin.lassartesse@gmail.com>
2024-12-05 09:04:21 +0400

    notifications system improved

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,35 @@
 class NotificationsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @notifications = current_user.notifications.recent
+    @unread_count = current_user.notifications.unread.count
+  end
+
+  def mark_as_read
+    notification = current_user.notifications.find(params[:id])
+    notification.mark_as_read!
+    
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back(fallback_location: root_path) }
+    end
+  end
+
+  def mark_all_as_read
+    current_user.notifications.mark_all_as_read(current_user.id)
+    
+    respond_to do |format|
+      format.turbo_stream { 
+        render turbo_stream: [
+          turbo_stream.update('notifications_count', ''),
+          turbo_stream.update('notifications_list', 
+            partial: 'notifications/notifications',
+            locals: { notifications: current_user.notifications.recent }
+          )
+        ]
+      }
+      format.html { redirect_back(fallback_location: root_path) }
+    end
+  end
 end
