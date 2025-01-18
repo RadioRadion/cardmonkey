38688868f5bf8cfcab7c6422ababf1d155809b16
RadioRadion <valentin.lassartesse@gmail.com>
2025-01-18 16:47:26 +0400

    fix: update database connection initializer to properly handle URL configs

--- config/initializers/database_connection.rb
+++ config/initializers/database_connection.rb
@@ -2,14 +2,19 @@ Rails.application.config.after_initialize do
   ActiveRecord::Base.connection_pool.disconnect!
 
   ActiveSupport.on_load(:active_record) do
-    config = ActiveRecord::Base.configurations.configs_for(env_name: Rails.env).first
-    config.pool = ENV['RAILS_MAX_THREADS'] || 5
-    
-    ActiveRecord::Base.establish_connection
+    database_url = ENV['DATABASE_URL']
+    if database_url
+      pool_size = ENV.fetch('RAILS_MAX_THREADS', 5).to_i
+      
+      db_config = ActiveRecord::Base.connection_pool.db_config
+      connection_options = db_config.configuration_hash.merge(pool: pool_size)
+      
+      ActiveRecord::Base.establish_connection(connection_options)
 
-    # Log database connection info
-    Rails.logger.info "Database Connection Pool Size: #{ActiveRecord::Base.connection.pool.size}"
-    Rails.logger.info "Database Connection Pool Checkout Timeout: #{ActiveRecord::Base.connection.pool.checkout_timeout}"
+      # Log database connection info
+      Rails.logger.info "Database Connection Pool Size: #{ActiveRecord::Base.connection.pool.size}"
+      Rails.logger.info "Database Connection Pool Checkout Timeout: #{ActiveRecord::Base.connection.pool.checkout_timeout}"
+    end
   end
 
   if defined?(PhusionPassenger)
