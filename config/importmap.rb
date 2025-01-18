70baddb881d8dc2c5ce213d6653a90a577636972
RadioRadion <valentin.lassartesse@gmail.com>
2025-01-18 15:23:06 +0400

    edit for deploy 2, importmaps call

--- config/importmap.rb
+++ config/importmap.rb
@@ -15,5 +15,9 @@ pin "channels", to: "channels/index.js", preload: true
 pin "channels/consumer", to: "channels/consumer.js", preload: true
 pin "channels/chatroom_channel", to: "channels/chatroom_channel.js", preload: true
 
-# Pin all JavaScript files
-pin_all_from "app/javascript", under: "javascript", preload: true
+# Pin JavaScript files
+pin "application", preload: true
+pin "cards/trigger", to: "cards/trigger.js", preload: true
+pin "channels/scroll", to: "channels/scroll.js", preload: true
+pin "forms/trigger_form", to: "forms/trigger_form.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers", preload: true
